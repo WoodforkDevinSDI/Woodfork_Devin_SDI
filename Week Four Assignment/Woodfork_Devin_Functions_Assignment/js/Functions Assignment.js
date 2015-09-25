124fae493b3292f77f3c4ab230f5ababb33c43a5
WoodforkDevinSDI <dmwoodfork@fullsail.edu>
2015-09-24 19:42:06 -0500

    Variables, Prompts, Validation
    Created the variables needed, prompt the user for values of variables
    and validated the answer with a while loop

--- Week Four Assignment/Woodfork_Devin_Functions_Assignment/js/Functions Assignment.js
+++ Week Four Assignment/Woodfork_Devin_Functions_Assignment/js/Functions Assignment.js
@@ -1,7 +1,31 @@
 /**
  * Created by DevinWoodfork on 9/24/15.
  */
+/* Woodfork, Devin
+   SDI Section 2
+   September 24, 2015
+ */
 
 
+//alert("It is linked properly!");
 
-alert("It is linked properly!");
+var vgConsole;
+var totalGames;
+var totalFunds;
+var costOfGames;
+
+vgConsole = prompt ("What is the price of the game console?");
+    while (vgConsole === "" || isNaN (vgConsole)){
+        vgConsole = prompt("Please enter the correct price of the console");
+    }
+totalGames = prompt ("How many games do you wish to buy with your game console?");
+    while (totalGames === "" || isNaN (totalGames)){
+        totalGames = prompt ("Please enter a number value for the amount of games you want.");
+    }
+totalFunds = prompt ("How much money do you have in total?");
+    while (totalFunds === "" || isNaN (totalFunds)){
+        totalFunds = prompt ("Please enter a number value for the amount of money you have in total.");
+    }
+costOfGames = prompt ("How much are games at the store?");
+    while (costOfGames === "" || isNaN (costOfGames)){
+    }
